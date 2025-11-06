c3b416bf4817a39634b5b31ff751899b0d46afb1
Maxim <36982547+Siphoin@users.noreply.github.com>
2025-11-06 12:33:35 +0500

    return background service

---
+++ Assets/SNEngine/Source/SNEngine/Services/BackgroundService.cs
@@ -0,0 +1,55 @@
+﻿using SNEngine.BackgroundSystem;
+using SNEngine.Debugging;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace SNEngine.Services
+{
+    [CreateAssetMenu(menuName = "SNEngine/Services/Bavkground Service")]
+    public class BackgroundService : ServiceBase
+    {
+        private IBackgroundRenderer _background;
+
+        public override void Initialize()
+        {
+            var background = Resources.Load<BackgroundRenderer>("Render/Background");
+
+            var screenBackground = Resources.Load<ScreenBackgroundRender>("Render/ScreenBackground");
+
+            var screenBackgroundPrefab = Instantiate(screenBackground);
+
+            screenBackgroundPrefab.name = screenBackground.name;
+
+            Object.DontDestroyOnLoad(screenBackgroundPrefab);
+
+            var backgroundPrefab = Instantiate(background);
+
+            backgroundPrefab.name = background.name;
+
+            Object.DontDestroyOnLoad(backgroundPrefab);
+
+            _background = backgroundPrefab;
+        }
+
+        public void ResetState()
+        {
+            _background.ResetState();
+        }
+
+        public void Set(Sprite sprite)
+        {
+            if (sprite is null)
+            {
+                NovelGameDebug.LogError($"Sprite for set background not seted. Check your graph");
+            }
+
+            _background.SetData(sprite);
+        }
+
+        public void Clear()
+        {
+            _background.Clear();
+        }
+    }
+
+}
