40e70b7dbc4179139e274eb57dabdc2cb9889187
Maxim <36982547+Siphoin@users.noreply.github.com>
2025-11-07 13:27:00 +0500

    Add SetMixerSoundNode

---
+++ Assets/SNEngine/Source/SNEngine/Audio/SetMixerSoundNode.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+
+namespace SNEngine.Audio
+{
+    public class SetMixerSoundNode : AudioNodeInteraction
+    {
+        [Input, SerializeField] private AudioMixerGroup _mixer;
+
+        protected override void Interact(AudioObject input)
+        {
+            var value = GetInputValue<AudioMixerGroup>(nameof(_mixer), _mixer);
+            input.Mixer = value;
+        }
+    }
+}
