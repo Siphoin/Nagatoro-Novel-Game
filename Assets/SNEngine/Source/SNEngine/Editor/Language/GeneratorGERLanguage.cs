d020d154c98f6bf97353c8c746640174f6a5b2db
Maxim <36982547+Siphoin@users.noreply.github.com>
2025-11-08 12:26:59 +0500

    add various languages for blank

---
+++ Assets/SNEngine/Source/SNEngine/Editor/Language/GeneratorGERLanguage.cs
@@ -0,0 +1,26 @@
+﻿using Cysharp.Threading.Tasks;
+using UnityEditor;
+
+namespace SNEngine.Editor.Language
+{
+    public static class GeneratorGERLanguage
+    {
+        [MenuItem("SNEngine/Language/Generate German Language")]
+        public static void GenerateBlankLanguageMenu()
+        {
+            _ = GenerateBlankAsync();
+        }
+
+        private static async UniTask GenerateBlankAsync()
+        {
+            try
+            {
+                await GeneratorLanguage.Generate("ger", "ger_flag.png");
+            }
+            catch (System.Exception ex)
+            {
+                UnityEngine.Debug.LogError($"[GeneratorENLanguage] Failed to generate blank language: {ex}");
+            }
+        }
+    }
+}
---
+++ Assets/SNEngine/Source/SNEngine/Editor/Language/GeneratorRULanguage.cs
@@ -0,0 +1,26 @@
+﻿using Cysharp.Threading.Tasks;
+using UnityEditor;
+
+namespace SNEngine.Editor.Language
+{
+    public static class GeneratorRULanguage
+    {
+        [MenuItem("SNEngine/Language/Generate RU Language")]
+        public static void GenerateBlankLanguageMenu()
+        {
+            _ = GenerateBlankAsync();
+        }
+
+        private static async UniTask GenerateBlankAsync()
+        {
+            try
+            {
+                await GeneratorLanguage.Generate("ru", "ru_flag.png");
+            }
+            catch (System.Exception ex)
+            {
+                UnityEngine.Debug.LogError($"[GeneratorENLanguage] Failed to generate blank language: {ex}");
+            }
+        }
+    }
+}
