0ca9487e5634bf3c7cff0a3eef4be374c3fa1af2
Maxim <36982547+Siphoin@users.noreply.github.com>
2025-11-09 13:40:29 +0500

    add localization string node

---
+++ Assets/SNEngine/Source/SNEngine/Localization/LocalizationStringNode.cs
@@ -0,0 +1,25 @@
+﻿using SiphoinUnityHelpers.XNodeExtensions.Varitables;
+
+namespace SNEngine.Localization
+{
+    public class LocalizationStringNode : StringNode, ILocalizationNode
+    {
+        public object GetOriginalValue()
+        {
+            return GetStartValue();
+        }
+
+        public object GetValue()
+        {
+            return GetStartValue();
+        }
+
+        public void SetValue(object value)
+        {
+            if (value is string str)
+            {
+                base.SetValue(str);
+            }
+        }
+    }
+}
