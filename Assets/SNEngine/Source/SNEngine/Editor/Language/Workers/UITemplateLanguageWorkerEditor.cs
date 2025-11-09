dfbc540c79b17771c3503f4e2246b0546f7ae6d4
Maxim <36982547+Siphoin@users.noreply.github.com>
2025-11-09 15:58:48 +0500

    add support localize of UI

---
+++ Assets/SNEngine/Source/SNEngine/Editor/Language/Workers/UITemplateLanguageWorkerEditor.cs
@@ -0,0 +1,64 @@
+﻿using Cysharp.Threading.Tasks;
+using SNEngine.Debugging;
+using SNEngine.IO;
+using System;
+using System.IO;
+using UnityEngine;
+
+namespace SNEngine.Editor.Language.Workers
+{
+    [CreateAssetMenu(menuName = "SNEngine/Editor/Language/Workers/UI Template Language Worker")]
+    public class UITemplateLanguageWorkerEditor : LanguageEditorWorker
+    {
+        public static string PathSave { get; set; }
+        private const string TEMPLATE_SOURCE_PATH = "Assets/SNEngine/Source/SNEngine/Resources/Editor/TextTemplates/ui_template.yaml";
+        private const string OUTPUT_FILE_NAME = "ui.yaml";
+
+        public override async UniTask<LanguageWorkerResult> Work()
+        {
+            LanguageWorkerResult result = new();
+
+            if (string.IsNullOrEmpty(PathSave))
+            {
+                string error = $"[{nameof(UITemplateLanguageWorkerEditor)}] PathSave not set";
+                NovelGameDebug.LogError(error);
+                result.Message = error;
+                result.State = LanguageWorkerState.Error;
+                return result;
+            }
+
+            string fullPath = Path.Combine(PathSave, OUTPUT_FILE_NAME);
+            string directory = Path.GetDirectoryName(fullPath);
+
+            if (!NovelDirectory.Exists(directory))
+            {
+                await NovelDirectory.CreateAsync(directory);
+            }
+
+            if (!File.Exists(TEMPLATE_SOURCE_PATH))
+            {
+                string error = $"[{nameof(UITemplateLanguageWorkerEditor)}] Template file not found: {TEMPLATE_SOURCE_PATH}";
+                NovelGameDebug.LogError(error);
+                result.Message = error;
+                result.State = LanguageWorkerState.Error;
+                return result;
+            }
+
+            try
+            {
+                byte[] templateBytes = await NovelFile.ReadAllBytesAsync(TEMPLATE_SOURCE_PATH);
+                await NovelFile.WriteAllBytesAsync(fullPath, templateBytes);
+            }
+            catch (Exception ex)
+            {
+                string error = $"[{nameof(UITemplateLanguageWorkerEditor)}] Failed to copy UI template: {ex.Message}";
+                NovelGameDebug.LogError(error);
+                result.Message = error;
+                result.State = LanguageWorkerState.Error;
+                return result;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Assets/SNEngine/Source/SNEngine/Localization/UILocalizationText.cs
@@ -0,0 +1,40 @@
+﻿using Cysharp.Threading.Tasks;
+using SiphoinUnityHelpers.XNodeExtensions.Attributes;
+using SNEngine.Services;
+using System.Collections;
+using TMPro;
+using UnityEngine;
+namespace SNEngine.Localization
+{
+    [RequireComponent(typeof(TextMeshProUGUI))]
+    public class UILocalizationText : MonoBehaviour
+    {
+        [SerializeField] private string _key;
+        [SerializeField, ReadOnly(ReadOnlyMode.Always)] private TextMeshProUGUI _component;
+
+        private LanguageService LanguageService => NovelGame.Instance.GetService<LanguageService>();
+
+        private void OnEnable()
+        {
+            LanguageService.OnLanguageLoaded += OnLanguageLoaded;
+        }
+
+        private void OnDisable()
+        {
+            LanguageService.OnLanguageLoaded -= OnLanguageLoaded;
+        }
+
+        private void OnLanguageLoaded(string languageCode)
+        {
+            _component.text = LanguageService.TransliteUI(_key);
+        }
+
+        private void OnValidate()
+        {
+            if (!_component)
+            {
+                _component = GetComponent<TextMeshProUGUI>();
+            }
+        }
+    }
+}
