2477c0526db7957f3d9b71d439e8971644ba10e8
Maxim <36982547+Siphoin@users.noreply.github.com>
2025-11-10 15:34:38 +0500

    add YAML nodes

---
+++ Assets/SNEngine/Source/SNEngine/YAML/FromYAMLNode.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+using SiphoinUnityHelpers.XNodeExtensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using XNode;
+using static XNode.Node;
+using Object = UnityEngine.Object;
+
+namespace SNEngine.YAML
+{
+    public class FromYAMLNode : BaseNode 
+    {
+        [Input(connectionType = ConnectionType.Override), SerializeField] private string _yaml;
+
+        [Output(ShowBackingValue.Never), SerializeField] private Object _result;
+
+        public override object GetValue(NodePort port)
+        {
+            if (!Application.isPlaying)
+            {
+                return base.GetValue(port);
+            }
+
+            var input = GetInputPort(nameof(_yaml));
+
+            string yaml = _yaml;
+
+            if (input.Connection != null)
+            {
+                yaml = (string)input.Connection.GetOutputValue();
+            }
+            SharpYaml.Serialization.Serializer serializer = new();
+            return serializer.Deserialize(yaml);
+        }
+    }
+}
---
+++ Assets/SNEngine/Source/SNEngine/YAML/ToYAMLNode.cs
@@ -0,0 +1,29 @@
+﻿using SiphoinUnityHelpers.XNodeExtensions;
+using UnityEngine;
+using XNode;
+using Object = UnityEngine.Object;
+
+namespace SNEngine.YAML
+{
+    public class ToYAMLNode : BaseNode
+    {
+        [Input(ShowBackingValue.Never, ConnectionType.Override), SerializeField] private Object _targetObject;
+
+        [Output(ShowBackingValue.Never), SerializeField] private string _yaml;
+
+        public override object GetValue(NodePort port)
+        {
+            if (!Application.isPlaying)
+            {
+                return base.GetValue(port);
+            }
+
+            var inputTarget = GetInputPort(nameof(_targetObject));
+
+            var value = inputTarget.Connection.GetOutputValue();
+
+            SharpYaml.Serialization.Serializer serializer = new();
+            return serializer.Serialize(value);
+        }
+    }
+}
