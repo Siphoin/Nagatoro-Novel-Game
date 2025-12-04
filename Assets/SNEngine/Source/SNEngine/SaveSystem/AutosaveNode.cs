24f29e0b8c7e5f0b701b82f8968ec152748a0a8f
Maxim <36982547+Siphoin@users.noreply.github.com>
2025-12-04 15:05:03 +0500

    add auto save node

---
+++ Assets/SNEngine/Source/SNEngine/SaveSystem/AutosaveNode.cs
@@ -0,0 +1,17 @@
+﻿using SiphoinUnityHelpers.XNodeExtensions.AsyncNodes;
+
+namespace SNEngine.SaveSystem
+{
+    public class AutosaveNode : AsyncNode
+    {
+        private const string NAME_SAVE = "autosave";
+
+        public override async void Execute()
+        {
+            base.Execute();
+            var service = NovelGame.Instance.GetService<SaveLoadService>();
+            await service.SaveCurrentState(NAME_SAVE);
+            StopTask();
+        }
+    }
+}
