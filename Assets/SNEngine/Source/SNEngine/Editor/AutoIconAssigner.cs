b8e046edd84b834f0feba55f0398c67477b9903b
Maxim <36982547+Siphoin@users.noreply.github.com>
2025-12-09 20:22:33 +0500

    Update AutoIconAssigner.cs

--- Assets/SNEngine/Source/SNEngine/Editor/AutoIconAssigner.cs
+++ Assets/SNEngine/Source/SNEngine/Editor/AutoIconAssigner.cs
@@ -3,38 +3,40 @@ using UnityEditor;
 using UnityEditor.Build;
 using UnityEditor.Build.Reporting;
 using SNEngine.Debugging;
-
-public class AutoIconAssigner : IPreprocessBuildWithReport
+namespace SNEngine.Editor
 {
-    public int callbackOrder => 0;
-
-    private const string ICON_PATH = "Assets/SNEngine/Source/SNEngine/Sprites/default_icon.png";
-
-    public void OnPreprocessBuild(BuildReport report)
+    public class AutoIconAssigner : IPreprocessBuildWithReport
     {
-        Texture2D[] currentIcons = PlayerSettings.GetIcons(NamedBuildTarget.Unknown, IconKind.Application);
+        public int callbackOrder => 0;
 
-        bool hasIcon = false;
-        if (currentIcons != null && currentIcons.Length > 0)
+        private const string ICON_PATH = "Assets/SNEngine/Source/SNEngine/Sprites/default_icon.png";
+
+        public void OnPreprocessBuild(BuildReport report)
         {
-            if (currentIcons[0] != null)
-            {
-                hasIcon = true;
-            }
-        }
+            Texture2D[] currentIcons = PlayerSettings.GetIcons(NamedBuildTarget.Unknown, IconKind.Application);
 
-        if (!hasIcon)
-        {
-            Texture2D newIcon = AssetDatabase.LoadAssetAtPath<Texture2D>(ICON_PATH);
-
-            if (newIcon != null)
+            bool hasIcon = false;
+            if (currentIcons != null && currentIcons.Length > 0)
             {
-                PlayerSettings.SetIcons(NamedBuildTarget.Unknown, new Texture2D[] { newIcon }, IconKind.Application);
-                NovelGameDebug.Log($"[AutoIconAssigner] Icon was missing. Assigned default application icon from: {ICON_PATH}");
+                if (currentIcons[0] != null)
+                {
+                    hasIcon = true;
+                }
             }
-            else
+
+            if (!hasIcon)
             {
-                NovelGameDebug.LogWarning($"[AutoIconAssigner] Icon missing and fallback texture not found at: {ICON_PATH}");
+                Texture2D newIcon = AssetDatabase.LoadAssetAtPath<Texture2D>(ICON_PATH);
+
+                if (newIcon != null)
+                {
+                    PlayerSettings.SetIcons(NamedBuildTarget.Unknown, new Texture2D[] { newIcon }, IconKind.Application);
+                    NovelGameDebug.Log($"[AutoIconAssigner] Icon was missing. Assigned default application icon from: {ICON_PATH}");
+                }
+                else
+                {
+                    NovelGameDebug.LogWarning($"[AutoIconAssigner] Icon missing and fallback texture not found at: {ICON_PATH}");
+                }
             }
         }
     }
